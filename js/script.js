14f312099770b9dbbcfe116c09ffaab316e5daf4
João Pedro Santos <jpedrosantosoliv@gmail.com>
2025-01-14 16:46:24 -0300

    added accessibility to FAQ

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 //Ativar links do Menu
-const links = document.querySelectorAll(".cabecalho li a")
+const links = document.querySelectorAll(".cabecalho li a");
 function ativarLink(link) {
     const url = window.location.href;
     const href = link.href;
     if (url.includes(href)) {
-        link.classList.add("ativo")
+        link.classList.add("ativo");
     }
 }
 links.forEach(ativarLink)
@@ -17,5 +17,23 @@ function ativarProduto(parametro) {
         elemento.checked = true;
     }
 }
-
 parametros.forEach(ativarProduto);
+
+//Perguntas frequentes
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativarPergunta(event){
+    const pergunta = event.currentTarget;
+    const controls = pergunta.getAttribute("aria-controls");
+    const resposta = document.getElementById(controls);
+
+    resposta.classList.toggle("ativo");
+    const ativo = resposta.classList.contains("ativo");
+    pergunta.setAttribute("aria-expanded", ativo);
+    console.log(ativo)
+}
+
+function eventosPerguntas(pergunta){
+    pergunta.addEventListener("click", ativarPergunta);
+}
+perguntas.forEach(eventosPerguntas)
